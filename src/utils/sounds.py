c2b3f7da20efb274715de9b23dfaa13a002211e6
StreakyFly <53691430+StreakyFly@users.noreply.github.com>
2024-02-28 17:21:32 +0100

    audio can be turned off now
    audio can be turned off now

--- src/utils/sounds.py
+++ src/utils/sounds.py
@@ -12,8 +12,9 @@ class Sounds:
     flap: List[pygame.mixer.Sound]
     collect_item: List[pygame.mixer.Sound]
 
-    def __init__(self) -> None:
-        pygame.mixer.set_num_channels(50)  # 50 channels just to be safe
+    def __init__(self, num_channels: int = 50) -> None:
+        pygame.mixer.set_num_channels(num_channels)
+        self.muted = False
 
         self.die = _load_sound('die.wav')
         self.hit = _load_sound('hit.wav')
@@ -22,19 +23,27 @@ class Sounds:
         self.flap = _load_sounds('flap', 2)
         self.collect_item = _load_sounds('items/collect_item', 4)
 
-    def play_random(self, sounds):
-        random.choice(sounds).play()
+    def set_muted(self, mute: bool) -> None:
+        self.muted = mute
+
+    def play(self, sound: pygame.mixer.Sound) -> None:
+        if self.muted:
+            return
+        sound.play()
+
+    def play_random(self, sounds: List[pygame.mixer.Sound]) -> None:
+        self.play(random.choice(sounds))
 
 
-def _load_sound(sound_name) -> pygame.mixer.Sound:
+def _load_sound(sound_name: str) -> pygame.mixer.Sound:
     return pygame.mixer.Sound(f'assets/audio/{sound_name}')
 
 
-def _load_item_sound(sound_name) -> pygame.mixer.Sound:
+def _load_item_sound(sound_name: str) -> pygame.mixer.Sound:
     return _load_sound(f'items/{sound_name}')
 
 
-def _load_sounds(sound_name, count) -> List[pygame.mixer.Sound]:
+def _load_sounds(sound_name: str, count: int) -> List[pygame.mixer.Sound]:
     sounds = []
     for num in range(1, count + 1):
         sounds.append(_load_sound(f'{sound_name}_{num}.wav'))
