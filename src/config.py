c64d8c2ab399ef40678c141f3d82bae276218362
StreakyFly <53691430+StreakyFly@users.noreply.github.com>
2024-07-07 15:35:48 +0200

    added warning if headless mode + capped fps
    cuz why would you want to enable headless mode and limit the fps

--- src/config.py
+++ src/config.py
@@ -5,10 +5,10 @@ from .ai.environments.env_types import EnvType
 
 
 class Config:
-    fps_cap = 30  # <-- change the FPS cap here; default = 30; 0 = uhh, it fucks up stuff, don't use it, do 9999 instead
-    debug = False  # <-- change if you want to enable debug mode
+    fps_cap = 30  # <-- change the FPS cap here; default = 30; no cap = 0 or a negative value
+    debug = True  # <-- change if you want to enable debug mode
     model = 'PPO'  # <-- change the model here (DQN or PPO)
-    mode = Mode.PLAY  # <-- change the mode here
+    mode = Mode.TEST_ENV  # <-- change the mode here
     env_type = EnvType.ENEMY_CLOUDSKIMMER  # <-- change environment type here
     run_id = None #"run_20240617_182959"  # <-- change the run id here (can be None in some cases)
 
@@ -18,14 +18,15 @@ class Config:
         'profile': False,  # profile the code
     }
 
-    # TODO should we warn user if headless is enabled but fps cap is low, like 30?
-
     @classmethod
     def verify_config(cls):
         if cls.model not in ['DQN', 'PPO']:
             raise ValueError(f"Invalid model: {cls.model}")
-        if cls.options['headless'] and not cls.options['mute']:
-            printc("WARNING! Headless mode is enabled but audio is not muted.", color="yellow")
+        if cls.options['headless']:
+            if not cls.options['mute']:
+                printc("WARNING! Headless mode is enabled but audio is not muted.", color="yellow")
+            if 0 < cls.fps_cap < 800:
+                printc("WARNING! Headless mode is enabled but FPS is capped. Use 0 for no FPS cap.", color="yellow")
         if cls.model == 'PPO' and cls.run_id is None and cls.mode not in [Mode.TRAIN, Mode.PLAY, Mode.TEST_ENV]:
             raise ValueError("The selected mode requires a run_id.")
         if cls.mode == Mode.TRAIN and cls.run_id is not None:
