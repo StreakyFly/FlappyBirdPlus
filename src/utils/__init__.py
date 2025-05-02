045822d04b76900e843657e1d541989edcab4f50
Miha Ciglar <miha.ciglar7@gmail.com>
2025-05-02 13:14:58 +0200

    fix(sounds): don't init pygame.mixer if config option mute is True
    So the game can even run (of course without sound, that is) on devices that don't have audio device access, such as HPC nodes.

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,7 +2,7 @@ from .game_config import GameConfig
 from .game_state import GameState, GameStateManager
 from .window import Window
 from .images import Images, load_image, animation_spritesheet_to_frames
-from .sounds import Sounds
+from .sounds import Sounds, DummySounds
 from .animation import Animation
 from .utils import get_random_value, get_mask, pixel_collision, rotate_on_pivot, printc
 from .image_style import apply_outline_and_shadow
